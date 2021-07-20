abe5e8460806bd48704958261d2ff16eb7856ebb
Vaalrhona <llouvet@easyfront.fr>
2021-07-20 16:46:33 +0200

    error handling

--- force-app/main/default/lwc/fileImport/fileImport.js
+++ force-app/main/default/lwc/fileImport/fileImport.js
@@ -1,6 +1,7 @@
 import { LightningElement } from 'lwc';
 import readRecords from '@salesforce/apex/FileImportController.readRecords';
-
+import { CloseActionScreenEvent } from 'lightning/actions';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 export default class FileImport extends LightningElement {
 
     get acceptedFormats() {
@@ -16,7 +17,20 @@ export default class FileImport extends LightningElement {
         })
         readRecords({fileIDs: fileIDs, objectApiName: 'Account'})
         .then(result => {
-            alert("No. of records uploaded : " + result);
+            this.dispatchEvent(new CloseActionScreenEvent());
+            const event = new ShowToastEvent();
+            if(result.success = 'true'){
+                event.title = 'Success';
+                event.message = result.message;
+                event.variant = 'success';
+            }
+            else{
+                event.title = 'There was a problem...';
+                event.message = result.message;
+                event.variant = 'warning';
+            }
+            this.dispatchEvent;
+            //alert("No. of records uploaded : " + result);
         })
         console.log(uploadedFiles);
     }
