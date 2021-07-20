f2e920b32ae35b76033c7167b3710d7c90a4240c
Vaalrhona <llouvet@easyfront.fr>
2021-07-20 16:06:58 +0200

    implementation of class in LWC

--- force-app/main/default/lwc/fileImport/fileImport.js
+++ force-app/main/default/lwc/fileImport/fileImport.js
@@ -1,14 +1,23 @@
 import { LightningElement } from 'lwc';
+import readRecords from '@salesforce/apex/FileImportController.readRecords';
 
 export default class FileImport extends LightningElement {
 
     get acceptedFormats() {
-        return ['.pdf', '.png'];
+        return ['.csv'];
     }
 
     handleUploadFinished(event) {
         // Get the list of uploaded files
         const uploadedFiles = event.detail.files;
-        alert("No. of files uploaded : " + uploadedFiles.length);
+        var fileIDs = [];
+        uploadedFiles.forEach(file => {
+            fileIDs.push(file.documentId);
+        })
+        readRecords({fileIDs: fileIDs, objectApiName: 'Account'})
+        .then(result => {
+            alert("No. of records uploaded : " + result);
+        })
+        console.log(uploadedFiles);
     }
 }
