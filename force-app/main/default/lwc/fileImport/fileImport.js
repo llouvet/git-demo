611db48b4deaf1f8830628a68f672be2f0fdac1b
Vaalrhona <llouvet@easyfront.fr>
2021-07-20 14:17:55 +0200

    First commit

---
+++ force-app/main/default/lwc/fileImport/fileImport.js
@@ -0,0 +1,14 @@
+import { LightningElement } from 'lwc';
+
+export default class FileImport extends LightningElement {
+
+    get acceptedFormats() {
+        return ['.pdf', '.png'];
+    }
+
+    handleUploadFinished(event) {
+        // Get the list of uploaded files
+        const uploadedFiles = event.detail.files;
+        alert("No. of files uploaded : " + uploadedFiles.length);
+    }
+}
